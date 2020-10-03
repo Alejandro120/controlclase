190953f8803a93507ec1580371dd46a7c3bca616
karma <alejogo543@gmail.com>
2020-10-03 18:14:19 +0200

    first commit

---
+++ src/test/java/com/controlclase/controlclase/ControlclaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlclase.controlclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlclaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
